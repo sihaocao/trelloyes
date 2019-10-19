027bfe32331226cfd1a2a1c74871ba511f2b49e6
scao <sihao.cao@gmail.com>
2019-10-19 15:09:13 -0400

    uploading tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,44 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import renderer from 'react-test-renderer';
+import Card from './card';
+import List from './list';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('Card component', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Card />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it('renders the UI as expected', () => {
+        const tree = renderer
+            .create(<Card title='First Card' content='Something...' />)
+            .toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+
+    it('renders without crashing', () => {
+      const div = document.createElement('div');
+      ReactDOM.render(<List />, div);
+      ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it('renders the UI as expected', () => {
+      const tree = renderer
+          .create(<List header='First List' cards={{ id: 'a', title: 'First card', content: 'lorem ipsum' }} />)
+          .toJSON();
+      expect(tree).toMatchSnapshot();
+  });
 });
--- src/list.js
+++ src/list.js
@@ -11,13 +11,14 @@ function List(props) {
                 </h2>
             </header>
             <div className='List-cards'>
-                {props.cards.map(card =>
-                    <Card
-                        key={card.id}
-                        title={card.title}
-                        content={card.content}
-                    />
-                )}
+                {props.cards.map((card) => {
+                    return (
+                        <Card
+                            key={card.id}
+                            title={card.title}
+                            content={card.content}
+                        />
+                )})}
                 <button
                     type='button'
                     className='List-add-button'
